f731e36dcd5c6438e67b21a2ee648bfedb34030d
JohannTzT <elementcraft888@gmail.com>
2023-01-04 15:44:57 -0300

    first

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
+require('dotenv').config();
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
@@ -15,10 +16,14 @@ app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 var token = process.env.TOKEN || 'token';
 var received_updates = [];
+
+console.log(process.env.TOKEN)
+
 app.get('/', function(req, res) {
   console.log(req);
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
+
 app.get(['/facebook', '/instagram'], function(req, res) {
   if (
     req.query['hub.mode'] == 'subscribe' &&
